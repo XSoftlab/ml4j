888b1bcfd3f47f5791a90ff7319e664232a3cc0e
XSoftlab <xsoftlab@163.com>
2015-02-05 23:19:12 +0800

    线性回归测试

--- examples/src/main/java/net/xsoftlab/ml4j/regression/TestLinearRegression.java
+++ examples/src/main/java/net/xsoftlab/ml4j/regression/TestLinearRegression.java
@@ -2,6 +2,7 @@ package net.xsoftlab.ml4j.regression;
 
 import java.io.IOException;
 
+import net.xsoftlab.ml4j.util.FeatureNormalize;
 import net.xsoftlab.ml4j.util.MatrixUtil;
 import net.xsoftlab.ml4j.util.TestUtil;
 
@@ -14,12 +15,18 @@ public class TestLinearRegression extends TestUtil {
 		tic();
 
 		logger.info("加载数据...\n");
-
 		String path = System.getProperty("user.dir") + "/resources/linearRegression/house.txt";
-		FloatMatrix[] matrixs = MatrixUtil.loadDataWithXY(path, ",", true);
+		FloatMatrix[] matrixs = MatrixUtil.loadDataWithXY(path, ",", false);
+
+		logger.info("特征标准化...\n");
+		FeatureNormalize featureNormalize = new FeatureNormalize(matrixs[0]);
+		FloatMatrix norMatrix = featureNormalize.normalize();
+
+		logger.info("添加截距项...\n");
+		FloatMatrix x = MatrixUtil.addIntercept(norMatrix);
 
 		logger.info("执行训练...\n");
-		LinearRegression lr = new LinearRegression(matrixs[0], matrixs[1], 0.01f, 1500);
+		LinearRegression lr = new LinearRegression(x, matrixs[1], 0.01f, 400);
 		FloatMatrix theta = lr.train();
 
 		logger.info("训练完成.\n \t theta = {}\n", theta);
