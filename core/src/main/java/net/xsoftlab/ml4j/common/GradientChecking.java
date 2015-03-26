d02317faf13ac2f4f9e7dc6979f8b2fbbf2785f7
XSoftlab <xsoftlab@163.com>
2015-03-26 23:21:59 +0800

    梯度校验，似乎哪里不太对

---
+++ core/src/main/java/net/xsoftlab/ml4j/common/GradientChecking.java
@@ -0,0 +1,90 @@
+package net.xsoftlab.ml4j.common;
+
+import net.xsoftlab.ml4j.model.BaseModel;
+
+import org.jblas.FloatMatrix;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 梯度校验
+ * 
+ * @author 王彦超
+ *
+ */
+public class GradientChecking {
+
+	protected BaseModel model;// 训练模型
+	protected int maxIter = 100;// 最大校验次数
+	private float delta = 1e-3f;// 梯度校验区间
+
+	protected FloatMatrix theta;// 参数
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * 初始化
+	 * 
+	 * @param model
+	 *            训练模型
+	 */
+	public GradientChecking(BaseModel model) {
+		super();
+		this.model = model;
+
+		this.theta = model.getInitTheta();
+	}
+
+	/**
+	 * 初始化
+	 * 
+	 * @param model
+	 *            训练模型
+	 * @param maxIter
+	 *            最大训练次数
+	 */
+	public GradientChecking(BaseModel model, int maxIter) {
+
+		this(model);
+		this.maxIter = maxIter;
+	}
+
+	public void check() {
+
+		float gest, error, avgError, sumError = 0;
+		float cost1, cost2;
+
+		model.compute(theta, 2);
+		FloatMatrix gradient = model.getGradient();// 初始梯度
+
+		FloatMatrix t0 = theta.dup();
+		FloatMatrix t1 = theta.dup();
+
+		maxIter = maxIter < theta.length ? maxIter : theta.length;
+
+		logger.debug("迭代次数 \t\t误差 \t\tgradient \tgest");
+		for (int i = 0; i < maxIter; i++) {
+			t0.put(i, t0.get(i) - delta);
+			t1.put(i, t1.get(i) + delta);
+
+			model.compute(t0, 1);
+			cost1 = model.getCost();
+
+			model.compute(t1, 1);
+			cost2 = model.getCost();
+
+			gest = (cost2 - cost1) / (2 * delta);
+			error = Math.abs(gradient.get(i) - gest);
+
+			sumError += error;
+			t0.put(i, t0.get(i) + delta);
+			t1.put(i, t1.get(i) - delta);
+
+			logger.debug("  {} \t   {}\t   {}\t   {}", new Object[] { i + 1, error, gradient.get(i), gest });
+		}
+
+		avgError = sumError / (float) maxIter;
+
+		logger.debug("平均误差： {}", avgError);
+	}
+}
---
+++ examples/src/main/java/net/xsoftlab/ml4j/common/Test_GradientChecking.java
@@ -0,0 +1,76 @@
+package net.xsoftlab.ml4j.common;
+
+import java.io.IOException;
+import java.util.Map;
+
+import net.xsoftlab.ml4j.model.BaseModel;
+import net.xsoftlab.ml4j.model.supervised.LinearRegression;
+import net.xsoftlab.ml4j.model.supervised.LogisticRegression;
+import net.xsoftlab.ml4j.util.MatrixUtil;
+import net.xsoftlab.ml4j.util.MnistLoader;
+import net.xsoftlab.ml4j.util.TestUtil;
+
+import org.jblas.FloatMatrix;
+
+/**
+ * 测试梯度校验
+ * 
+ * @author 王彦超
+ *
+ */
+public class Test_GradientChecking extends TestUtil {
+
+	public static void main(String[] args) throws IOException {
+
+		TestLinearRegression();
+	}
+
+	public static void TestLinearRegression() throws IOException {
+
+		tic();
+
+		logger.info("加载数据...\n");
+		String path = RESOURCES_PATH + "/ufldl/newVersion/ex1/housing.data";
+		FloatMatrix matrix = MatrixUtil.loadData(path, "\\s+", false);
+
+		logger.info("打乱数据...\n");
+		matrix = MatrixUtil.shuffle(matrix);
+
+		logger.info("分成训练集和测试集...\n");
+		FloatMatrix train = matrix.getRange(0, 400, 0, matrix.columns);
+		FloatMatrix train_x = train.getRange(0, train.rows, 0, train.columns - 1);
+		FloatMatrix train_y = train.getRange(0, train.rows, train.columns - 1, train.columns);
+
+		logger.info("训练集特征标准化...\n");
+		FeatureNormalize trainNormalize = new FeatureNormalize(train_x, true);
+		train_x = trainNormalize.normalize();
+
+		logger.info("模型初始化...\n");
+		BaseModel model = new LinearRegression(train_x, train_y);
+
+		logger.info("执行梯度校验...\n");
+		GradientChecking gc = new GradientChecking(model);
+		gc.check();
+
+		toc();
+	}
+
+	public static void TestLogisticRegression() throws IOException {
+
+		tic();
+
+		logger.info("加载数据...\n");
+		Map<String, FloatMatrix[]> map = MnistLoader.load(true);
+		FloatMatrix[] train = map.get("train");
+
+		logger.info("模型初始化...\n");
+		BaseModel model = new LogisticRegression(train[0], train[1]);
+
+		logger.info("执行梯度校验...\n");
+		GradientChecking gc = new GradientChecking(model);
+		gc.check();
+
+		toc();
+	}
+
+}
